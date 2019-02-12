ec61688527bdabaa97070e0dea478f88074f4a3a
nunogit <nunomsilvanunes@gmail.com>
2019-02-12 14:50:35 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -3,22 +3,6 @@ FROM openlink/virtuoso-opensource-7:latest
 
 MAINTAINER BiGCaT
 
-ENV PORT 8890
-ENV PORT 1111
+RUN mkdir -p /data
 
-#RUN apt-get update \
-#    && apt-get install -y wget \
-#    && apt-get install -y zip
-
-#RUN mkdir -p /dataload/data
-#WORKDIR /dataload/data
-#RUN wget  http://data.wikipathways.org/current/rdf/wikipathways-20190110-rdf-void.ttl
-#RUN wget  http://data.wikipathways.org/current/rdf/wikipathways-20190110-rdf-gpml.zip
-#RUN wget  http://data.wikipathways.org/current/rdf/wikipathways-20190110-rdf-wp.zip
-#RUN zip -d *.zip
-#RUN find . -name *.ttl -exec cat > ../all.ttl {} \;
-#WORKDIR /database/
-
-EXPOSE 8890 1111
-
-#ENTRYPOINT service apache2 start && /opt/bridgedb/bridgedb-2.3.0/startup.sh
+VOLUME /data
--- doDocker.sh
+++ doDocker.sh
@@ -17,12 +17,12 @@ cd ../../
 
 docker build --no-cache -t "develop" .
 #docker build -t p .
-docker tag develop bigcatum/virtuoso-wikipathways:develop
-docker run -d --env DBA_PASSWORD=dba --name loadVirtuoso --volume `pwd`/dataload/:/database  bigcatum/virtuoso-wikipathways:develop
+docker tag develop bigcatum/wikipathways-virtuoso:develop
+docker run -d --env DBA_PASSWORD=dba --name loadVirtuoso --volume `pwd`/dataload/:/database/data/  bigcatum/wikipathways-virtuoso:develop
 
-docker exec -ti loadVirtuoso isql -P dba -S 1111 exec="DB.DBA.TTLP_MT (file_to_string_output ('/opt/virtuoso-opensource/database/all.ttl'), 'http://rdf.wikipathways.org/');"
+sleep 10 #dirty solution to wait for Virtuoso to fully start
 
-
+docker exec -ti loadVirtuoso isql -P dba -S 1111 exec="DB.DBA.TTLP_MT (file_to_string_output ('/opt/virtuoso-opensource/database/data/all.ttl'), 'http://rdf.wikipathways.org/');"
 
 #sudo docker tag -f p dtlfair/search
-##sudo docker push bigcatum/virtuoso-wikipathways:develop
+docker push bigcatum/wikipathways-virtuoso:develop
